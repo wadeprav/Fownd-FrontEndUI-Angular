4f43b87cf74a66e3a837b3334d33fcce3aa772c8
Kiran Divate <divate26@gmail.com>
2022-04-17 08:28:28 +0530

    feature/login: update calender

--- src/app/common/appointment-calender/appointment-calender.component.ts
+++ src/app/common/appointment-calender/appointment-calender.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { formatDate, FullCalendarComponent, CalendarOptions } from '@fullcalendar/angular';
+import { formatDate, CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-appointment-calender',
@@ -11,18 +11,21 @@ export class AppointmentCalenderComponent implements OnInit {
   ngOnInit(): void {
     
   }
+  Events = [];
+
   calendarOptions: CalendarOptions = {
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      right: 'dayGridMonth'
     },
     initialView: 'dayGridMonth',
     weekends: true,
     editable: true,
     selectable: true,
     selectMirror: true,
-    dayMaxEvents: true
+    dayMaxEvents: true,
+    buttonIcons: false
   };
 
   str = formatDate(new Date(), {
@@ -31,7 +34,9 @@ export class AppointmentCalenderComponent implements OnInit {
     day: 'numeric'
   });
 
-  handleDateClick(arg: any) {
-    alert('date click! ' + arg.dateStr)
+  handleDateClick() {
+    console.log('date click! ');
+    // this.Events.push(arg.dateStr);
+    return 'arg';
   }
 }
